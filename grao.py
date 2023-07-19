6ab1ee3e9b1c8b8666d6cadb5abc976a3038f8cc
Gabriel Brognoli <gbrognoli53@gmail.com>
2023-07-19 16:23:39 -0300

    Create grao.py

---
+++ grao.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Load the data
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+train_generator = train_datagen.flow_from_directory(
+    '/path/to/train/images',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='binary')
+
+validation_datagen = ImageDataGenerator(
+    rescale=1./255)
+
+validation_generator = validation_datagen.flow_from_directory(
+    '/path/to/validation/images',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='binary')
+
+# Create the model
+model = Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(224, 224, 3)),
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=10,
+    validation_data=validation_generator,
+    validation_steps=10)
+
+# Save the model
+model.save('model.h5')
